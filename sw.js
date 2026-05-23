6b43cafc80e29b6f1c19dbb389e7833366447627
rayjiaye123 <pjwgsm89ng@privaterelay.appleid.com>
2026-05-23 21:46:10 +0800

    Switch UI to night sky theme

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "intj-knowledge-shell-v16";
+const CACHE_NAME = "intj-knowledge-shell-v17";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.json", "./favicon.ico"];
 
 self.addEventListener("install", (event) => {
